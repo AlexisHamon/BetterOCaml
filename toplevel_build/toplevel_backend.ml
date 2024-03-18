ccad00c9561b6180ac3f4835d6caa08033cfbb71
Alexis Hamon <alexis.hamon@ens-rennes.fr>
2024-03-18 18:49:21 +0000

    ✨ mitigate OCaml toplevel error/exception catching

--- toplevel_build/toplevel_backend.ml
+++ toplevel_build/toplevel_backend.ml
@@ -26,5 +26,90 @@ let setup_toplevel () =
   Sys.interactive := true;
   ()
 
+open Js_of_ocaml_compiler.Stdlib
+
+let refill_lexbuf s p ppf buffer len =
+  if !p = String.length s
+  then 0
+  else
+    let len', nl =
+      try String.index_from s !p '\n' - !p + 1, false
+      with _ -> String.length s - !p, true
+    in
+    let len'' = min len len' in
+    Bytes.blit_string ~src:s ~src_pos:!p ~dst:buffer ~dst_pos:0 ~len:len'';
+    (match ppf with
+    | Some ppf ->
+        Format.fprintf ppf "%s" (Bytes.sub_string buffer ~pos:0 ~len:len'');
+        if nl then Format.pp_print_newline ppf ();
+        Format.pp_print_flush ppf ()
+    | None -> ());
+    p := !p + len'';
+    len''
+
+module JsooTopPpx = struct
+  let ppx_rewriters = ref []
+
+  let () = Ast_mapper.register_function := fun _ f -> ppx_rewriters := f :: !ppx_rewriters
+
+  let preprocess_structure str =
+    let open Ast_mapper in
+    List.fold_right !ppx_rewriters ~init:str ~f:(fun ppx_rewriter str ->
+        let mapper = ppx_rewriter [] in
+        mapper.structure mapper str)
+
+  let _preprocess_signature str =
+    let open Ast_mapper in
+    List.fold_right !ppx_rewriters ~init:str ~f:(fun ppx_rewriter str ->
+        let mapper = ppx_rewriter [] in
+        mapper.signature mapper str)
+
+  let preprocess_phrase phrase =
+    let open Parsetree in
+    match phrase with
+    | Ptop_def str -> Ptop_def (preprocess_structure str)
+    | Ptop_dir _ as x -> x
+end
+
+module JsooTopError = struct
+  let loc = function
+    | Syntaxerr.Error x -> Some (Syntaxerr.location_of_error x)
+    | Lexer.Error (_, loc)
+    | Typecore.Error (loc, _, _)
+    | Typetexp.Error (loc, _, _)
+    | Typeclass.Error (loc, _, _)
+    | Typemod.Error (loc, _, _)
+    | Typedecl.Error (loc, _)
+    | Translcore.Error (loc, _)
+    | Translclass.Error (loc, _)
+    | Translmod.Error (loc, _) -> Some loc
+    | _ -> None
+end
+
+exception RuntimeError
+
 let execute printval ?pp_code ?highlight_location pp_answer s =
-  JsooTop.execute printval ?pp_code:pp_code ?highlight_location:highlight_location pp_answer s
+  let lb = Lexing.from_function (refill_lexbuf s (ref 0) pp_code) in
+  (try
+     while true do
+       try
+         let phr = !Toploop.parse_toplevel_phrase lb in
+         let phr = JsooTopPpx.preprocess_phrase phr in
+         match Toploop.execute_phrase printval pp_answer phr with
+          | true  -> () 
+          | false ->  
+            Printf.eprintf "Uncaught exception: %s\n" (Printexc.to_string RuntimeError);
+            flush stderr
+       with
+       | End_of_file -> raise End_of_file
+       | x ->
+           (match highlight_location with
+           | None -> ()
+           | Some f -> (
+               match JsooTopError.loc x with
+               | None -> ()
+               | Some loc -> f loc));
+           Errors.report_error Format.err_formatter x
+     done
+   with End_of_file -> ());
+  flush_all ()
